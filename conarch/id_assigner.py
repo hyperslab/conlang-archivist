45b29c80801b5d3320e8b7d81a9e7bcbb9ec1988
hyperslab <silasjm6@gmail.com>
2023-02-24 11:20:49 -0500

    Copy configurable database location functionality into id_assigner

--- conarch/id_assigner.py
+++ conarch/id_assigner.py
@@ -1,6 +1,25 @@
 import sqlite3
+import platformdirs
+import configparser
+import os
 
-DB_FILE_PATH = 'languages.db'
+# TODO need to figure out the import spaghetti so this doesn't have to be duplicated from db
+CONFIG_DIRECTORY = platformdirs.user_config_dir('ConlangArchivist', appauthor=False, roaming=True)
+if not os.path.exists(CONFIG_DIRECTORY):
+    os.makedirs(CONFIG_DIRECTORY)
+CONFIG_FILE_PATH = os.path.join(CONFIG_DIRECTORY, 'config.ini')
+if not os.path.isfile(CONFIG_FILE_PATH):
+    DB_DIRECTORY = platformdirs.user_data_dir('ConlangArchivist', appauthor=False, roaming=True)  # default
+    config = configparser.ConfigParser()
+    config['Database'] = {}
+    config['Database']['Directory'] = DB_DIRECTORY
+    with open(CONFIG_FILE_PATH, 'w') as configfile:
+        config.write(configfile)
+else:
+    config = configparser.ConfigParser()
+    config.read(CONFIG_FILE_PATH)
+    DB_DIRECTORY = config['Database']['Directory']
+DB_FILE_PATH = os.path.join(DB_DIRECTORY, 'languages.db')
 
 max_word_id = -1
 
